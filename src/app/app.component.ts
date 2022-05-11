e68df86eebb4727d236583cb9fce2cfe541f4fd1
Sekar <sekarfriends2006@gmail.com>
2022-05-11 22:51:49 +0530

    chore: Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'apple';
+  para:boolean=false
+  
+  Fruit=['Apple','Orange','Mango','Disable']
+  Number=['One','Two','Three']
+  update(fruitName: any){
+    switch(fruitName){
+      case 'Disable':
+      this.para=true;
+      break;
+      case 'Apple':
+      this.para=false;
+      break;
+      case 'Orange':
+      this.para=false;
+      break;
+      case 'Mango':
+      this.para=false;
+      break;
+    }
+
+  }
 }
